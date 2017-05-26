0b2c8580f6d49a2b987684d9d3b6fec8435f5829
StevenJV <StevenVore@tnwinc.com>
2017-05-26 09:53:04 -0400

    module 2: introduction

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Hello
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Your name:");
+      string name = Console.ReadLine();
+
+      Console.WriteLine("How many hours of sleep did you get last night? ");
+      int hoursOfSleep = int.Parse(Console.ReadLine());
+
+      Console.WriteLine("Hello, " + name);
+      if (hoursOfSleep > 8)
+      {
+        Console.WriteLine("you are well rested.");
+      }
+      else
+      {
+        Console.WriteLine("you need more sleep.");
+      }
+    }
+  }
+}
