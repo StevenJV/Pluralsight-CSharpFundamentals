281337616823e65ccbc5c7379d9a297ffd7fdc1a
StevenJV <StevenVore@tnwinc.com>
2017-05-26 09:56:40 -0400

    module2: classes and objects

---
+++ Grades/Grades/GradeBook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+  class GradeBook
+  {
+    public readonly List<float> _grades; 
+    public GradeBook()
+    {
+      _grades = new List<float>();
+    }
+
+    public void AddGrade(float grade)
+    {
+
+      _grades.Add(grade);
+    }
+  }
+}
