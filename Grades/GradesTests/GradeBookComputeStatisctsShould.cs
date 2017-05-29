bbeda566b5d44b8e9a04f2bd7d7c6e16186a1cd5
StevenJV <StevenVore@tnwinc.com>
2017-05-29 12:14:39 -0400

    Unit Testing (my version)

---
+++ Grades/GradesTests/GradeBookComputeStatisctsShould.cs
@@ -0,0 +1,75 @@
+﻿namespace Grades
+{
+  using FluentAssertions;
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Threading;
+  using System.Threading.Tasks;
+  using Xunit;
+  using Xunit.Extensions;
+
+
+  public class GradeBookComputeStatisctsShould
+  {
+    [Fact]
+    public void Return_average_of_multiple_grades()
+    {
+      GradeBook gradebook = new GradeBook();
+      gradebook.AddGrade(75f);
+      gradebook.AddGrade(77f);
+      GradeStatistics gradebookStats = gradebook.ComputeStatistics();
+      Assert.Equal(76f, gradebookStats.Avg());
+    }
+
+    [Fact]
+    public void Return_average_zero_if_no_grades()
+    {
+      GradeBook gradebook = new GradeBook();
+      GradeStatistics gradebookStats = gradebook.ComputeStatistics();
+      Assert.Equal(0f, gradebookStats.Avg());
+    }
+
+    [Fact]
+    public void Return_average_of_single_grade_if_only_one_grade()
+    {
+      GradeBook gradebook = new GradeBook();
+      gradebook.AddGrade(75f);
+      GradeStatistics gradebookStats = gradebook.ComputeStatistics();
+      Assert.Equal(75f, gradebookStats.Avg());
+    }
+    [Fact]
+    public void Return_max_zero_if_no_grades()
+    {
+      GradeBook gradebook = new GradeBook();
+      GradeStatistics gradebookStats = gradebook.ComputeStatistics();
+      Assert.Equal(0f, gradebookStats.Max());
+    }
+    [Fact]
+    public void Return_min_zero_if_no_grades()
+    {
+      GradeBook gradebook = new GradeBook();
+      GradeStatistics gradebookStats = gradebook.ComputeStatistics();
+      Assert.Equal(0f, gradebookStats.Min());
+    }
+    [Fact]
+    public void Return_min_of_multiple_grades()
+    {
+      GradeBook gradebook = new GradeBook();
+      gradebook.AddGrade(75f);
+      gradebook.AddGrade(77f);
+      GradeStatistics gradebookStats = gradebook.ComputeStatistics();
+      Assert.Equal(75f, gradebookStats.Min());
+    }
+    [Fact]
+    public void Return_max_of_multiple_grades()
+    {
+      GradeBook gradebook = new GradeBook();
+      gradebook.AddGrade(75f);
+      gradebook.AddGrade(77f);
+      GradeStatistics gradebookStats = gradebook.ComputeStatistics();
+      Assert.Equal(77f, gradebookStats.Max());
+    }
+
+  }
+}
---
+++ Grades/GradesTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GradesTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GradesTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("316ab9f5-15cd-445c-bbbc-de51e5e445d3")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
