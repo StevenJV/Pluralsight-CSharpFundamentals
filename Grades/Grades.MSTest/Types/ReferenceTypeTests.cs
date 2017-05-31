6f8a595f7ba4e38bd70cf6129da599bf160967ff
StevenJV <StevenVore@tnwinc.com>
2017-05-31 10:40:55 -0400

    Immutability (value and reference types). DateTime and Arrays are value type; String is reference but behaves like value

--- Grades/Grades.MSTest/Types/ReferenceTypeTests.cs
+++ Grades/Grades.MSTest/Types/ReferenceTypeTests.cs
@@ -11,6 +11,40 @@ namespace Grades.MSTest.Types
   public class TypeTests
   {
     [TestMethod]
+    public void UsingArrays()
+    {
+      var grades = new float[3];
+      AddGrades(grades);
+      Assert.AreEqual(89.1f, grades[1]);
+    }
+
+    private void AddGrades(float[] grades)
+    {
+      // arrays are reference type, so the contents of the array object to which grades points can be changed
+      grades[1] = 89.1f;
+    }
+
+    [TestMethod]
+    public void AddDaysToDateTime()
+    {
+      DateTime date = new DateTime(2017, 1, 1);
+      date.AddDays(1);
+      // Assert.AreEqual(2,date.Day); // fails, as DateTime is a value type and therefore immutable
+      DateTime newdate = date.AddDays(1);
+      Assert.AreEqual(2, newdate.Day);
+    }
+
+    [TestMethod]
+    public void UppercaseString()
+    {
+      string name = "steven";
+      name.ToUpper(); // doesn't actually do anything useful. Even though string is a reference type, it behaves like a value type
+      // Assert.AreEqual("STEVEN", name); // fails as name never got changed.
+      name = name.ToUpper(); // re-points name to location of returned (upper-case) string
+      Assert.AreEqual("STEVEN", name);
+    }
+
+    [TestMethod]
     public void ValueTypesPassByValue()
     {
       int x = 46;
@@ -20,7 +54,7 @@ namespace Grades.MSTest.Types
 
     private void IncrementNumber(int i)
     {
-       i++;
+      i++;
     }
 
     [TestMethod]
