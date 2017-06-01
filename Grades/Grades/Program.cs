76d1558323fb025a677097220a9059df849113b3
StevenJV <StevenVore@tnwinc.com>
2017-06-01 13:47:13 -0400

    data setup extracted to methods

--- Grades/Grades/Program.cs
+++ Grades/Grades/Program.cs
@@ -11,29 +11,46 @@ namespace Grades
   {
     static void Main(string[] args)
     {
-      GradeBook stevensGradeBook = new GradeBook("Steven");
-      stevensGradeBook.AddGrade(91);
-      stevensGradeBook.AddGrade(95.9f);
-      GradeBook bobsGradebook = new GradeBook();
-      bobsGradebook.NameChanged += OnNameChanged;
-      bobsGradebook.Name = "Bob";
-      bobsGradebook.AddGrade(75);
-      bobsGradebook.AddGrade(55);
+      var stevensGradeBook = CreateStevensGradeBook();
+      var bobsGradebook = CreateBobsGradebook();
 
       GradeStatistics stevensStats = stevensGradeBook.ComputeStatistics();
       GradeStatistics bobsStats = bobsGradebook.ComputeStatistics();
 
-      Console.WriteLine($"\n{stevensGradeBook.Name}'s grade stats:");
+      Console.WriteLine($"\n{stevensGradeBook.Name}'s grades:");
+      stevensGradeBook.WriteGrades(Console.Out);
       WriteResult("minimum", (int)stevensStats.Min());
       WriteResult("maximum", (int)stevensStats.Max());
       WriteResult("average", stevensStats.Avg());
       WriteResult("grade", stevensStats.Description);
 
-      Console.WriteLine($"\n{bobsGradebook.Name}'s grade stats:");
+      Console.WriteLine($"\n{bobsGradebook.Name}'s grades:");
+      bobsGradebook.WriteGrades(Console.Out);
       WriteResult("minimum", (int)bobsStats.Min());
       WriteResult("maximum", (int)bobsStats.Max());
       WriteResult("average", bobsStats.Avg());
       WriteResult("grade", bobsStats.Description);
+    }
+
+    private static GradeBook CreateBobsGradebook()
+    {
+      GradeBook bobsGradebook = new GradeBook();
+      bobsGradebook.NameChanged += OnNameChanged;
+      bobsGradebook.Name = "Bob";
+      bobsGradebook.AddGrade(75);
+      bobsGradebook.AddGrade(76);
+      bobsGradebook.AddGrade(60);
+      bobsGradebook.AddGrade(78);
+      bobsGradebook.AddGrade(55);
+      return bobsGradebook;
+    }
+
+    private static GradeBook CreateStevensGradeBook()
+    {
+      GradeBook stevensGradeBook = new GradeBook("Steven");
+      stevensGradeBook.AddGrade(91);
+      stevensGradeBook.AddGrade(95.9f);
+      return stevensGradeBook;
     }
 
     private static void OnNameChanged(object sender, NameChangedEventArgs args)
